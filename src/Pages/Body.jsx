affcee232bd069a85e60601d78e56896969bf58d
Sanket1005 <sanketshrestha@Sankets-MacBook-Air.local>
2026-04-07 10:30:45 +0545

    React

---
+++ src/Pages/Body.jsx
@@ -0,0 +1,6 @@
+function Body(){
+    return(
+        <h1>body</h1>
+    )
+}
+export default Body
---
+++ src/Pages/Nav.jsx
@@ -0,0 +1,7 @@
+function Nav (){
+    return(
+        <h1> Nav bar </h1>
+    )
+    }
+
+    export default Nav
